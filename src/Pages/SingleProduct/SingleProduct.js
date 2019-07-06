aa92b6d2ed0d45bd3e39f28cede6d9b73562f177
Anja <rasoloanja@gmail.com>
2019-07-05 23:46:02 -0400

    Init single product page

--- src/Pages/SingleProduct/SingleProduct.js
+++ src/Pages/SingleProduct/SingleProduct.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './SingleProduct.css';
-
+import Button from '../../components/Button/Button';
 
 
 
@@ -10,7 +10,8 @@ class SingleProduct extends Component {
         description: '',
         category:'',
         price: '',
-        image: ''
+        image: '',
+        date: '',
     }
 
     componentDidMount(){
@@ -30,12 +31,14 @@ class SingleProduct extends Component {
             return res.json()
         })
         .then(resData => {
+            const date = resData.product.createdAt.slice(0, 10);
             this.setState({
                 title: resData.product.title,
                 price: resData.product.price,
                 category: resData.product.category,
-                image: 'http://localhost:8000' + resData.product.imageUrl,
-                description: resData.product.description
+                image: 'http://localhost:8000/' + resData.product.imageUrl,
+                description: resData.product.description,
+                date: date
 
             })
         })
@@ -46,10 +49,30 @@ class SingleProduct extends Component {
 
     render() {
         return (
-            <section className="single-product">
-                <h1>{this.state.title}</h1>
-                <h1>{this.state.price}</h1>
-                <h1>{this.state.title}</h1>
+            <section className="single-product flex-centered-row">
+                <article>
+                      
+                    <div className="single-product__details">
+                        <h1 className="single-product__details__title">{this.state.title}</h1>
+                        <div className="single-product__details__date">{this.state.date}</div>
+                        <p>{this.state.description}</p>
+                        <div className="single-product__details__price flex-centered-row">${this.state.price}</div>
+                        <div className="single-product__details__cta flex-centered-row">
+                            <Button color='primary'>
+                                Add to cart
+                            </Button>
+                        </div>
+                    </div> 
+                    <div className="single-product__image" 
+                    style={{
+                        backgroundImage: `url('${this.state.image}')`,
+                        backgroundSize: 'contain',
+                        backgroundPosition: 'center',
+                        backgroundRepeat: 'no-repeat'
+                    }}/>
+                    
+                </article>
+                
             </section>
         )
     }
