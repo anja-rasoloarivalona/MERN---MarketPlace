162af992c40b9fd23e44fea861439e575d599e59
Anja <rasoloanja@gmail.com>
2019-07-11 16:17:39 -0400

    Handled loading state

--- src/Pages/Shop/ShopIndex/ShopIndex.js
+++ src/Pages/Shop/ShopIndex/ShopIndex.js
@@ -4,6 +4,7 @@ import Product from '../../../components/Product/Product';
 import bg from '../../../assets/img/bg.jpg';
 import ShopLayout from '../Shop';
 import Paginator from '../../../components/Paginator/Paginator';
+import Spinner from '../../../components/Spinner/Spinner';
 
 
 
@@ -19,7 +20,8 @@ import Paginator from '../../../components/Paginator/Paginator';
         priceMax: 99999, 
         sortBy: 'latest',
         totalProducts: 0,
-        currentPage: 1
+        currentPage: 1,
+        loading: false
     }
 
 
@@ -35,6 +37,8 @@ import Paginator from '../../../components/Paginator/Paginator';
  
 
     loadProductsHandler = direction => {
+
+        this.setState({loading: true})
 
         if(direction){
             this.setState( {products: []} )
@@ -68,7 +72,8 @@ import Paginator from '../../../components/Paginator/Paginator';
                 if(this._isMounted === true) {
                     this.setState({
                         products: resData.products,
-                        totalProducts: resData.totalProducts
+                        totalProducts: resData.totalProducts,
+                        loading: false
                     })
                 }
 
@@ -77,7 +82,8 @@ import Paginator from '../../../components/Paginator/Paginator';
             })
             .catch( err => {
                 if(err){
-                    console.log(err)
+                    console.log(err);
+                    this.setState({loading: false})
                 }
                
             })
@@ -99,31 +105,14 @@ import Paginator from '../../../components/Paginator/Paginator';
             () => this.loadProductsHandler())
     }
 
-    render() {      
-        return (
-            
-                <ShopLayout 
-                    priceMax ={this.state.priceMax}
-                    priceMin = {this.state.priceMin}
-                    productPriceRequested={this.state.productPriceRequested}
-                    inputRangeChangeHandler={this.inputRangeChangeHandler}
-                    minPrice = {this.state.productPriceRequested.min}
-                    maxPrice ={this.state.productPriceRequested.max}
-                    onChangeComplete = {this.onChangeComplete}
-                    sortbyhandler = {this.sortbyhandler}>
-                        
-                    
-
-                    <section className="shop__intro" style={{
-                                    backgroundImage: `url('${bg}')`,
-                                    backgroundSize: 'cover',
-                                    backgroundPosition: 'center',
-                                    backgroundRepeat: 'no-repeat'
-                            }} >
-                    
-                    </section>              
-                    <section className="shop--index"> 
-                        <Paginator onRequestPreviousPage={this.loadProductsHandler.bind(this, 'previous')}
+    render() {  
+        
+        let products;
+        if(this.state.loading === true){
+            products = <Spinner />
+        } else {
+            products = (
+                <Paginator onRequestPreviousPage={this.loadProductsHandler.bind(this, 'previous')}
                                                 onRequestNextPage={this.loadProductsHandler.bind(this, 'next')}
                                                 lastPage={Math.ceil(this.state.totalProducts / 5)}
                                                 currentPage={this.state.currentPage}>               
@@ -147,7 +136,33 @@ import Paginator from '../../../components/Paginator/Paginator';
                                                 />                        
                                     )                                                      
                                 })}  
-                        </Paginator>                             
+                        </Paginator> 
+            )
+        }
+        return (
+            
+                <ShopLayout 
+                    priceMax ={this.state.priceMax}
+                    priceMin = {this.state.priceMin}
+                    productPriceRequested={this.state.productPriceRequested}
+                    inputRangeChangeHandler={this.inputRangeChangeHandler}
+                    minPrice = {this.state.productPriceRequested.min}
+                    maxPrice ={this.state.productPriceRequested.max}
+                    onChangeComplete = {this.onChangeComplete}
+                    sortbyhandler = {this.sortbyhandler}>
+                        
+                    
+
+                    <section className="shop__intro" style={{
+                                    backgroundImage: `url('${bg}')`,
+                                    backgroundSize: 'cover',
+                                    backgroundPosition: 'center',
+                                    backgroundRepeat: 'no-repeat'
+                            }} >
+                    
+                    </section>              
+                    <section className="shop--index"> 
+                           {products}                         
                     </section>
                     </ShopLayout>
         
---
+++ src/components/Spinner/Spinner.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Spinner.css';
+
+const spinner = () => (
+    <div class="spinner flex-centered-row"><div></div><div></div><div></div></div>
+    )
+
+
+export default spinner;
+
