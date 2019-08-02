bd5e87d6f9ffff9cf365af65078dbb90bf897bf7
Anja <rasoloanja@gmail.com>
2019-08-01 22:41:07 -0400

    Added logic to display the various delivery dates dynamically

--- src/Pages/Checkout/Delivery/Delivery.js
+++ src/Pages/Checkout/Delivery/Delivery.js
@@ -3,31 +3,130 @@ import './Delivery.css';
 import Button from '../../../components/Button/Button';
 import UserInfo from '../Recap/UserInfo/UserInfo';
 
+const WEEKDAY = new Array(7)
+WEEKDAY[0] =  "Sunday";
+WEEKDAY[1] = "Monday";
+WEEKDAY[2] = "Tuesday";
+WEEKDAY[3] = "Wednesday";
+WEEKDAY[4] = "Thursday";
+WEEKDAY[5] = "Friday";
+WEEKDAY[6] = "Saturday";
+
+
  class Delivery extends Component {
 
+   
+
     state = {
-        delivery: [
-            "Wednesday, Aug. 14 - Tuesday, Aug. 20",
-            10
-            ],
+        delivery: [],
         firstOption: true,
-        secondOption: false 
+        secondOption: false,
+
+
+        cheapest: [],
+        fastest: []
     }
 
-    componentDidMount(){
-        console.log('did mount', this.state.delivery)
+    componentWillMount(){ 
+        let today = new Date();
+
+        let cheapestFirstDate = new Date();
+        let cheapestLastDate = new Date();
+        let fastestDate = new Date();
+
+        cheapestFirstDate.setDate(today.getDate() + 10);
+        cheapestLastDate.setDate(today.getDate() + 15);
+        fastestDate.setDate(today.getDate() + 2)
+
+        let deliveryDatesArray = [
+            {
+                name: 'cheapFirstDate',
+                date: cheapestFirstDate
+            }, 
+            {
+                name: 'cheapLastDate',
+                date: cheapestLastDate
+            },
+            {
+                name: 'asapDate',
+                date: fastestDate
+            }
+        ]
+
+        let weekday = WEEKDAY;
+        let deliveryDatesOutput = [];
+
+        deliveryDatesArray.map( d => {
+            let weekDayInNumber = d.date.getDay();
+            let dateData = {
+                name: d.name,
+                templateLitteral: `${weekday[weekDayInNumber]}, ${d.date.toString().slice(4, 7)} ${d.date.getDate()}`       
+            }
+            deliveryDatesOutput.push(dateData)
+        })
+
+        let cheapFirstDateOutput = deliveryDatesOutput.find( o => o.name === 'cheapFirstDate');
+        let cheapLastDateOutPut = deliveryDatesOutput.find( o => o.name === 'cheapLastDate');
+        let cheapestDateDelivery = `${cheapFirstDateOutput.templateLitteral} - ${cheapLastDateOutPut.templateLitteral}`;
+        let cheapestDeliveryMethod = [cheapestDateDelivery, 10];
+
+        let fastestDateOutPut = deliveryDatesOutput.find( o => o.name === 'asapDate');     
+        let fastestDeliveryMethod = [fastestDateOutPut.templateLitteral, 20]
+
+        this.setState({
+            delivery: {
+                    date: cheapestDeliveryMethod[0],
+                    price: cheapestDeliveryMethod[1]},
+            cheapest: {
+                    date: cheapestDeliveryMethod[0],
+                    price: cheapestDeliveryMethod[1]
+            },
+            fastest: {
+                    date: fastestDeliveryMethod[0],
+                    price: fastestDeliveryMethod[1]
+            }
+
+        }, () => {
+            console.log(this.state)
+        })  
     }
+
+
 
     toggle(event){
-            this.setState({
-                delivery: event.target.value,
+
+        let date;
+        let price;
+
+        if(event === 'fastest'){
+            date = this.state.fastest.date;
+            price = this.state.fastest.price
+        }
+
+        if(event === 'cheapest'){
+            date = this.state.cheapest.date;
+            price= this.state.cheapest.price
+        }
+
+            this.setState( prevState => ({
+               ...prevState,
+                delivery: {
+                    ...prevState.delivery,
+                    date: date,
+                    price: price
+                },
                 firstOption: !this.state.firstOption,
                 secondOption: !this.state.secondOption
-            }, console.log(this.state))
+            }), () => {
+                console.log(this.state.delivery)
+            })
+            
+            
     }
 
 
     render() {
+
         return (
             <div className="delivery">
                 <UserInfo />
@@ -39,33 +138,26 @@ import UserInfo from '../Recap/UserInfo/UserInfo';
                         <div className="delivery__option">    
                                 <input type="checkbox" 
                                        name="cheapest" 
-                                       value={[
-                                        "Wednesday, Aug. 14 - Tuesday, Aug. 20",
-                                        10
-                                        ]}
-
+                                       value={this.state.cheapest}
                                        checked={this.state.firstOption}
-                                       onClick={this.toggle.bind(this)}/>
+                                       onChange={this.toggle.bind(this, 'cheapest')}/>
 
                                 <label for="cheapest">
-                                    <div>Wednesday, Aug. 14 - Tuesday, Aug. 20</div>
-                                    <div>$10</div>
+                                    <div>{`${this.state.cheapest.date}`}</div>
+                                    <div>${`${this.state.cheapest.price}`}</div>
                                 </label>
                         </div>
 
                         <div className="delivery__option">     
                                 <input type="checkbox" 
                                        name="fastest" 
-                                       value={[
-                                        "Monday, Aug. 5",
-                                        20
-                                        ]}
+                                       value={[this.state.fastest]}
                                        checked={this.state.secondOption}
-                                       onClick={this.toggle.bind(this)}/>
+                                       onChange={this.toggle.bind(this, 'fastest')}/>
 
                                 <label for="fastest">
-                                    <div>Monday, Aug. 5</div>
-                                    <div>$20</div>
+                                    <div>{`${this.state.fastest.date}`}</div>
+                                    <div>${`${this.state.fastest.price}`}</div>
                                 </label>
                         </div>
                         <Button type="submit"
