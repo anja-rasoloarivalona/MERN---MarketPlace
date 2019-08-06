6ba9eb7602b0c75764676b9b9239585a7cdce4e9
Anja <rasoloanja@gmail.com>
2019-08-06 13:36:51 -0400

    Styled admin orders

--- src/Pages/Admin/AdminOrders/AdminOrders.js
+++ src/Pages/Admin/AdminOrders/AdminOrders.js
@@ -45,19 +45,59 @@ class AdminOrders extends Component {
                 <div className="admin-order"
                      key={order.shortId}>
                     <h2>Order: {order.shortId}</h2>
-                    <div className="admin-order__products-list">
-                        {
-                            order.cart.items.map( prod => {
-                               return (
-                                   <div className="admin-order__product">
-                                       <div>{prod.product.title}</div>
-                                       <div>{prod.product.price}</div>
-                                   </div>
-                               )
 
-                            })
-                        }
+                    <div className="admin-order__body">
+
+
+
+                        <div className="admin-order__products-list">
+                            <div className="admin-order__title">Items</div>
+                            {
+                                order.cart.items.map( prod => {
+                                return (
+                                    <div className="admin-order__details">
+                                        <div>{prod.product.title}</div>
+                                        <div>${prod.product.price}</div>
+                                    </div>
+                                )
+                                })
+                            }
+
+                            <div className="admin-order__details admin-order__details--price">
+                                <span>Subtotal: </span><span>${order.cart.subTotalPrice}</span>
+                            </div>
+                            <div className="admin-order__details">
+                                <span>Taxes: </span><span>${order.cart.taxes}</span>
+                            </div>
+                            <div className="admin-order__details">
+                                <span>Delivery: </span><span>${order.cart.deliveryFee}</span>
+                            </div>
+                            <div className="admin-order__details">
+                                <span className="admin-order__details--total" >Total: </span> <span className="admin-order__details--total">${order.cart.totalPrice}</span>
+                            </div>
+
+
+
+                        </div>
+
+
+
+                        <div className="amdin-order__delivery">
+                            <div className="admin-order__title">Delivery</div>
+
+                            <div className="amdin-order__delivery__details">
+                                <span>{order.address.fullname}</span>
+                                <span>{order.address.address1} {order.address.address2}</span>
+                                <div>{order.address.city}, {order.address.zip}</div>
+                                <div>{order.address.state}</div>
+                            </div>
+
+                            <div>
+                                <span className="admin-order__delivery__date">Date: {order.deliveryDate}</span>
+                            </div>
+                        </div>
                     </div>
+                    
                 </div> 
            )
         })
